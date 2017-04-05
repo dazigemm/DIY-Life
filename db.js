fa90dde211f3a3a309565acb52c277c0bf22b19c
D <dazigemm@gmail.com>
2017-04-05 11:27:34 -0400

    trying to setup db

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 // db.js
 const mongoose = require('mongoose');
 const URLSlugs = require('mongoose-url-slugs');
-
+// need to fix
 const User = new mongoose.Schema({
 	name: String,
 	password: String
 });
 
 const Event = new mongoose.Schema({
-	before: Event,
-	after: [Event],
+	before: Object,//Event,
+	after: [Object],
 	storyLine: String
 });
 
@@ -17,7 +17,7 @@ const Story = new mongoose.Schema({
 	writers: [User],
 	title: String,
 	events: [Event],//maybe save beginning of story?
-	isFinished: boolean
+	isFinished: String
 });
 
 Story.plugin(URLSlugs('title'));
@@ -26,4 +26,23 @@ mongoose.model('User', User);
 mongoose.model('Event', Event);
 mongoose.model('Story', Event);
 
-mongoose.connect('mongodb://localhost/final');
+// is the environment variable, NODE_ENV, set to PRODUCTION?
+if (process.env.NOD_ENV === 'PRODUCTION') {
+	// if we're in PRODUCTION mode, then read the configuration from a file
+	// use blocking file io to do this...
+	var fs = require('fs');
+	var path = require('path');
+	var fn = path.join(__dirname, 'config.json');
+	var data = fs.readFileSync(fn);
+
+	// our configuration file will be in json, so parse it and set the 
+	// connection string appropriately!
+	var conf = JSON.parse(data);
+	var dbconf = conf.dbcontf;
+}
+else {
+	// if we're not in PRODUCTION mode, then use
+	dbconf = 'mongodb://localhost/final';
+}
+
+mongoose.connect(dbconf);
