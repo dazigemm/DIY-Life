5acfdcc7ef790daa69e7d4f8260d3958e31abd8c
D <dazigemm@gmail.com>
2017-04-27 18:34:40 -0400

    using phantom js to scrape links

--- hello.js
+++ hello.js
@@ -1,12 +1,42 @@
+var fs = require('fs');
+var path = 'output.csv';
+
 console.log("Hello World!");
 var urls = ['http://linserv2.cims.nyu.edu:21531/login',
 	'http://linserv2.cims.nyu.edu:21531/register', 
-	'http://linserv2.cims.nyu.edu:21531'];
+	'http://linserv2.cims.nyu.edu:21531/'];
 
 var page = require('webpage').create();
+//*
+page.open('http://linserv2.cims.nyu.edu:21531/', function(status) {
+		
+	const links = page.evaluate(function() {
+		return [].map.call(document.getElementsByTagName("a"), function (lin) {
+			return lin.href;
+		});
+	});
+	console.log("Links Found: ");
+	console.log(links.join('\n'));
+	//console.log(links);
+	//console.log("List of Stories:");
+	//console.log(links.length);
+	/*for (let i = 0; i < links.length; i++) {//doesn't work
+		console.log(links[i].innerHTML);
+	}*/
+	fs.write(path, links, 'w');
+	phantom.exit();
+});
+
+/*
 function handlePage (file) {
 	page.open(file, function(status) {
 		console.log(file + " Retrieval Status: " + status);
+		const links = page.evaluate(function() {
+			return document.getElementsByTagName("a");
+		});
+		for (let i = 0; i < links.length; i++) {
+			urls.push(links[i].href);
+		}/
 		setTimeout(nextPage, 100);
 	});
 }
@@ -17,3 +47,4 @@ function nextPage() {
 	handlePage(file);
 }
 nextPage();
+*/
